d452c3cdba29fc6a754a1d7dd1c98e4134a4b45b
Nikelin96 <n.revutskyi@gmail.com>
2021-11-06 12:02:48 +0200

    Initial commit

---
+++ src/GrpcClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            var request = new HelloRequest { Name = "Nik" };
+
+            var reply = await client.SayHelloAsync(request);
+
+            Console.WriteLine(reply.Message);
+
+            Console.ReadLine();
+        }
+    }
+}
